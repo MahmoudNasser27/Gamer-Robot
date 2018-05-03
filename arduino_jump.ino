65799372e2bc4d3a9220cfe456a4f32aacb57aa5
MahmoudNasser27 <38930354+MahmoudNasser27@users.noreply.github.com>
2018-05-03 16:27:29 +0200

    Add files via upload

---
+++ arduino_jump.ino
@@ -0,0 +1,24 @@
+ #include <Servo.h>
+
+ Servo myservo;  // create servo object to control a servo
+ 
+void setup() {
+  myservo.attach(9);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  
+  if(Serial.available() > 0){       //wait for jump command from PC
+   Serial.read();
+
+  myservo.write(130);              //move the servo horn to press the button  
+  delay(230);
+  myservo.write(90);               //return to release the button
+  delay(150);
+  while(Serial.available() > 0)  Serial.read();           //to remove any bending commands
+    
+  }
+  
+}
